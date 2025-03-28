74ee500b180874e2c9221654dea6180a3b69c55e
Suraj-art-tech <suraj.pandey01@nagarro.com>
2025-03-28 08:38:46 +0000

    8th  Commit

--- src/app/page.js
+++ src/app/page.js
@@ -30,6 +30,7 @@ export default function Home() {
       {
         method: 'GET',
         headers: {
+          'Host': `${window.location.host}`,
           'Content-Type': 'application/json',
         },
         credentials: 'include', // Ensures cookies are included
